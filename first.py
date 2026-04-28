49283bcba26156083081014c549642d82ba8e5b6
Mohammad Javid Miakhil <javidmiakhil63@gmail.com>
2026-04-28 15:02:42 +0300

    string concatanation

--- first.py
+++ first.py
@@ -10,3 +10,17 @@ print(my_list)
 # this is a comment
 day = "Today's day is Monday"
 print(day)
+print(len(day))
+weather = "The weather is sunny"
+#string concatenation
+dayAndWeather = day + " " +"and "+ weather
+print(dayAndWeather)
+
+#concatenation string with numbers
+number = 10
+numberString = "The number is "
+# this will cause an error because we cannot concatenate a string with a number
+# numberAndString = numberString + number   
+# to fix this we can convert the number to a string
+numberAndString = numberString + str(number)
+print(numberAndString)
