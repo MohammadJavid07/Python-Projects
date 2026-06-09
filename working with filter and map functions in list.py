d8f1b58abd284a70ecd38d892a110a39da792f93
Mohammad Javid Miakhil <javidmiakhil63@gmail.com>
2026-06-09 11:48:39 +0300

    using filter method in list

--- working with filter and map functions in list.py
+++ working with filter and map functions in list.py
@@ -6,3 +6,11 @@ fahrenheit = list(map(celsius_to_fahrenheit, celsuis))
 print(fahrenheit)
 
 
+numbers = list(range(1, 100))
+# Using filter to get even numbers from the list
+def is_even(number):
+    return number % 2 == 0
+even_numbers = list(filter(is_even, numbers))
+print(even_numbers)
+
+
