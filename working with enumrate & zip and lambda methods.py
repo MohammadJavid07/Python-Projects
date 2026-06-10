336601f793057ab2689f89b38f6794a5bd787be7
Mohammad Javid Miakhil <javidmiakhil63@gmail.com>
2026-06-10 10:55:49 +0300

    lambda function

--- working with enumrate & zip and lambda methods.py
+++ working with enumrate & zip and lambda methods.py
@@ -11,3 +11,7 @@ for index, developer in enumerate(developers, 1):
 provinces = ['Ontario', 'Quebec', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan', 'Nova Scotia', 'New Brunswick', 'Newfoundland and Labrador', 'Prince Edward Island']
 for developer, province in zip(developers, provinces):
     print(f"{developer} : lives in {province}")
+
+# Using lambda to find the length of each developer's name
+name_lengths = list(map(lambda x: len(x), developers))
+print("Name lengths:", name_lengths)
